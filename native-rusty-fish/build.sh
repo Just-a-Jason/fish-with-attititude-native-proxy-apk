be7114c54b6bf50461efaa9cf72db832d9d8a9a8
Just-a-Jason <antek.wojcieszyk@gmail.com>
2026-09-02 11:04:29 +0200

    build essencials

--- native-rusty-fish/build.sh
+++ native-rusty-fish/build.sh
@@ -1,2 +1,10 @@
-# Requires package cargo ndk
-cargo ndk build --release .
+#!/bin/bash
+set -e
+
+export ANDROID_NDK_HOME=../Ndk/android-ndk-r25c
+
+cargo ndk -t armv7-linux-androideabi --platform 19 build --release -q
+
+cp ./target/armv7-linux-androideabi/release/librusty_fish.so ../lib/libgame.so
+
+echo "Patch library build!"
--- native-rusty-fish/src/lib.rs
+++ native-rusty-fish/src/lib.rs
@@ -1,14 +1,45 @@
-use jni::sys::jclass;
+use jni::sys::{jclass, jstring};
+use std::ffi::c_void;
+
+const RTLD_NEXT: *mut c_void = -1isize as *mut c_void;
+
+type BasePopupCtor = unsafe extern "C" fn(*mut c_void, *mut c_void);
+type This = *mut c_void;
+type JsonNode = *mut c_void;
 
 #[no_mangle]
 pub extern "system" fn Java_com_helloworld_HelloWorld_hello(
     env: jni::JNIEnv,
     _class: jclass,
-) -> jni::sys::jstring {
+) -> jstring {
     let msg = "Hello from rust arm!".to_owned();
 
     env.new_string(msg)
-    .expect("Couldn't create java string!")
+        .expect("Couldn't create java string!")
         .into_raw()
 }
 
+unsafe fn call_original_and_hook(symbol: &[u8], this: This, json_node: JsonNode) {
+    println!("[Proxy] Przechwycono BasePopup! this: {:p}", this);
+
+    let real_func_ptr = libc::dlsym(RTLD_NEXT, symbol.as_ptr() as *const _);
+
+    if !real_func_ptr.is_null() {
+        let original_ctor: BasePopupCtor = std::mem::transmute(real_func_ptr);
+        original_ctor(this, json_node);
+    } else {
+        eprintln!("[Proxy] BŁĄD: Nie znaleziono oryginalnego symbolu w libgame_real.so!");
+    }
+
+    println!("[Proxy] BasePopup został utworzony. Dopisuję własną logikę!");
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn _ZN9BasePopupC1EP8JSONNode(this: *mut c_void, json_node: *mut c_void) {
+    call_original_and_hook(b"_ZN9BasePopupC1EP8JSONNode\0", this, json_node);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn _ZN9BasePopupC2EP8JSONNode(this: *mut c_void, json_node: *mut c_void) {
+    call_original_and_hook(b"_ZN9BasePopupC2EP8JSONNode\0", this, json_node);
+}
